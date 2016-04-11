b3168f32c2fb35435bd4cbb630778c57bb2fe607
dtrinh888 <dtrinh888@gmail.com>
2016-04-10 19:54:49 -0700

    Pre-Loading Objects

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 router.get('/posts', function(req, res, next){
 	Post.find(function(err, posts){
@@ -29,6 +30,26 @@ router.post('/posts', function(req, res, next){
 
 		res.json(post);
 	});
+});
+
+router.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if(err) {
+			return next(err);
+		}
+		if(!post) {
+			return next(new Error('can\'t find post'));
+		}
+
+		req.post = post;
+		return next();
+	});
+});
+
+router.get('/posts/:post', function(req, res){
+	res.json(req.post);
 });
 
 module.exports = router;

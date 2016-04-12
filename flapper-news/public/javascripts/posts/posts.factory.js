e3d85c56d2e7ff7fe29e140eb3e21053326c5c1f
dtrinh888 <dtrinh888@gmail.com>
2016-04-11 23:07:11 -0700

    upvoting posts enabled

--- flapper-news/public/javascripts/posts/posts.factory.js
+++ flapper-news/public/javascripts/posts/posts.factory.js
@@ -1,13 +1,29 @@
+// needed to inject $http
 app.factory('posts', ['$http', [function($http){
 	
 	var o = {
 		posts: [],
 	};
 	
+	// to retrieve posts in the posts service within angularApp.js
 	o.getall = function(){
 		return $http.get('/posts').success(function(data){
 			angular.copy(data. o.posts);
 		});
+	};
+
+	// method to create new posts
+	o.create = function(post){
+		return $http.post('/posts', post).success(function(data){
+			o.posts.push(data);
+		});
+	};
+
+	o.upvote = function(post) {
+		return $http.put('/posts' + post._id + '/upvote')
+			.success(function(data){
+				post.upvotes += 1;
+			});
 	};
 	
 	return o;
